91b32cc83955e98d8f679fbafe6a33cc365bc78a
rafaelhbastos <rafaelhenribastos12@gmail.com>
2022-11-07 22:04:01 -0300

    Dealing with form

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -6,18 +6,23 @@ import LoginCreate from './LoginCreate';
 import LoginPasswordLost from './LoginPasswordLost';
 import LoginPasswordReset from './LoginPasswordReset';
 import { UserContext } from '../../UserContext';
+import styles from '../../CSS/Login.module.css'
 
 const Login = () => {
   const { login } = useContext(UserContext)
 
   if (login === true) return <Navigate to="/conta" />
   return (
-    <Routes>
-      <Route path='/' element={<LoginForm/>}/>
-      <Route path='/criar' element={<LoginCreate/>} />
-      <Route path='/perdeu' element={<LoginPasswordLost/>} />
-      <Route path='/resetar' element={<LoginPasswordReset/>} />
-    </Routes>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path='/' element={<LoginForm/>}/>
+          <Route path='/criar' element={<LoginCreate/>} />
+          <Route path='/perdeu' element={<LoginPasswordLost/>} />
+          <Route path='/resetar' element={<LoginPasswordReset/>} />
+        </Routes>
+      </div>
+    </section>
   )
 }
 
