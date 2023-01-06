336d72911f0969ff8b46fe0ea204ea330373096c
rafaelhbastos <rafaelhenribastos12@gmail.com>
2023-01-06 19:03:56 -0300

    Final details

--- src/Components/Login/LoginPasswordReset.js
+++ src/Components/Login/LoginPasswordReset.js
@@ -44,7 +44,7 @@ const LoginPasswordReset = () => {
   }, []);
 
   return (
-    <div>
+    <section className="animeLeft">
       <Head title="Resete a senha"/> 
       <h1 className="title">Resete a senha</h1>
       <form onSubmit={handleSubmit}>
@@ -61,7 +61,7 @@ const LoginPasswordReset = () => {
         )}
       </form>
       <Error error={error} />
-    </div>
+    </section>
   );
 };
 
--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -1,13 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Head from '../Helper/Head'
+import useFetch from './../../Hooks/UseFetch';
+import { STATS_GET } from './../../api';
+import Loading from './../Helper/Loading';
+import Error from './../Helper/Error';
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'));
 
 const UserStats = () => {
+  const {data, error, loading, request} = useFetch();
+
+  useEffect(() => {
+    async function getData() {
+      const {url, options} = STATS_GET();
+      await request(url, options);
+    }
+    getData();
+  }, [request]);
+
+  if (loading) return <Loading />
+  if (error) return <Error error={error}/>
+  if (data)
   return ( 
     <div>
-      <Head title="Estatísticas"/>
-      UserStats
+      <React.Suspense fallback={<div></div>}>
+        <Head title="Estatísticas"/>
+        <UserStatsGraphs data={data}/>
+      </React.Suspense>
     </div>
-  )
+  );
+  else return null;
 }
 
 export default UserStats
